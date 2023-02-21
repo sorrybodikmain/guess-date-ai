6e1b16baa1205adaa20f9b34af6417c3d2d3f37e
Bogdan Mazur <zochit21@gmail.com>
2023-02-21 16:50:49 +0200

    added optimazed code

--- optimazed-main.py
+++ optimazed-main.py
@@ -1,3 +1,4 @@
+import numpy as np
 from keras.layers import Input, Dense, Dropout
 from keras.models import Model
 from keras.optimizers import Adam
@@ -13,41 +14,41 @@ dates = ["2020-03-22", "2020-03-23", "2020-04-26", "2020-05-25", "2020-06-26", "
          "2022-12-11", "2023-01-25", "2023-01-26", "2023-02-14", "2023-02-15", "2023-02-16", "2023-02-17", "2023-02-18",
          "2023-02-19", "2023-02-20"]
 
-print('Convert to datetime objects')
+# Convert to datetime objects and normalize to have a range of [0, 1]
 date_objects = [datetime.strptime(date, '%Y-%m-%d') for date in dates]
+normalized_dates = np.array(
+    [(date - min(date_objects)) / (max(date_objects) - min(date_objects)) for date in date_objects])
 
-print('Normalize datetime objects to have a range of [0, 1]')
-min_date = min(date_objects)
-max_date = max(date_objects)
-normalized_dates = [(date - min_date) / (max_date - min_date) for date in date_objects]
-
-print('Define the neural network')
+# Define the neural network
 inputs = Input(shape=(1,))
 x = Dense(256, activation='relu')(inputs)
 x = Dropout(0.2)(x)
 x = Dense(128, activation='relu')(x)
 x = Dropout(0.2)(x)
 outputs = Dense(1, activation='linear')(x)
-
 model = Model(inputs=inputs, outputs=outputs)
 
-print('Compile the model')
+# Compile the model
 model.compile(optimizer=Adam(learning_rate=0.001), loss='mse')
 
-print('Train the model')
-model.fit(x=normalized_dates[:-1], y=normalized_dates[1:], epochs=500, verbose=0)
-print('End of model training')
-print('Predict the next 5 dates')
+# Train the model
+model.fit(x=normalized_dates[:-1, np.newaxis], y=normalized_dates[1:, np.newaxis], epochs=500, verbose=0)
+
+# Predict the next 5 dates
 predicted_dates = []
 current_date = datetime.now()
 for _ in range(5):
     # Normalize the current date
-    normalized_current_date = (current_date - min_date) / (max_date - min_date)
+    normalized_current_date = (current_date - min(date_objects)) / (max(date_objects) - min(date_objects))
+
     # Predict the next date
-    predicted_normalized_date = model.predict([[normalized_current_date]])[0][0]
+    predicted_normalized_date = model.predict(np.array([[normalized_current_date]]))[0][0]
+
     # Denormalize the predicted date
-    predicted_date = min_date + timedelta(days=predicted_normalized_date * (max_date - min_date).days)
+    predicted_date = min(date_objects) + timedelta(
+        days=predicted_normalized_date * (max(date_objects) - min(date_objects)).days)
     predicted_dates.append(predicted_date.strftime('%Y-%m-%d'))
+
     # Update the current date to be the predicted date for the next iteration
     current_date = predicted_date
 
