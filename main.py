e489a13c1f0783b862eb686211efdc531310b3da
Bogdan Mazur <zochit21@gmail.com>
2023-02-21 22:56:29 +0200

    added fast api and fixed cors

--- main.py
+++ main.py
@@ -1,54 +1,63 @@
+import numpy as np
 from keras.layers import Input, Dense, Dropout
 from keras.models import Model
 from keras.optimizers import Adam
 from datetime import datetime, timedelta
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+from starlette.responses import JSONResponse
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 
-# Input data
-dates = ["2020-03-22", "2020-03-23", "2020-04-26", "2020-05-25", "2020-06-26", "2020-07-28", "2020-09-01", "2020-10-05",
-         "2020-11-07", "2020-12-09", "2021-02-25", "2021-03-19", "2021-04-21", "2021-05-26", "2021-05-27", "2021-05-28",
-         "2021-05-29", "2021-05-30", "2021-05-31", "2021-07-13", "2021-08-19", "2021-09-22", "2021-11-07", "2021-12-08",
-         "2022-01-25", "2022-03-01", "2022-04-06", "2022-05-09", "2022-06-16", "2022-07-20", "2022-08-29", "2022-11-11",
-         "2022-12-11", "2023-01-25", "2023-01-26", "2023-02-14", "2023-02-15", "2023-02-16", "2023-02-17", "2023-02-18",
-         "2023-02-19", "2023-02-20"]
+app = FastAPI()
 
-print('Convert to datetime objects')
-date_objects = [datetime.strptime(date, '%Y-%m-%d') for date in dates]
+origins = [
+    "http://localhost",
+    "https://localhost:3000",
+    "http://localhost:3000",
+    "https://fntracker.pp.ua",
+]
 
-print('Normalize datetime objects to have a range of [0, 1]')
-min_date = min(date_objects)
-max_date = max(date_objects)
-normalized_dates = [(date - min_date) / (max_date - min_date) for date in date_objects]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-print('Define the neural network')
-inputs = Input(shape=(1,))
-x = Dense(256, activation='relu')(inputs)
-x = Dropout(0.2)(x)
-x = Dense(128, activation='relu')(x)
-x = Dropout(0.2)(x)
-outputs = Dense(1, activation='linear')(x)
 
-model = Model(inputs=inputs, outputs=outputs)
+@app.get("/")
+async def root():
+    return 'hello world'
 
-print('Compile the model')
-model.compile(optimizer=Adam(learning_rate=0.001), loss='mse')
 
-print('Train the model')
-model.fit(x=normalized_dates[:-1], y=normalized_dates[1:], epochs=500, verbose=0)
-print('End of model training')
-print('Predict the next 5 dates')
-predicted_dates = []
-current_date = datetime.now()
-for _ in range(5):
-    # Normalize the current date
-    normalized_current_date = (current_date - min_date) / (max_date - min_date)
-    # Predict the next date
-    predicted_normalized_date = model.predict([[normalized_current_date]])[0][0]
-    # Denormalize the predicted date
-    predicted_date = min_date + timedelta(days=predicted_normalized_date * (max_date - min_date).days)
-    predicted_dates.append(predicted_date.strftime('%Y-%m-%d'))
-    # Update the current date to be the predicted date for the next iteration
-    current_date = predicted_date
+@app.options("/dates")
+async def calc_dates3(data: Request):
+    return 'gfgfdgfdgdf'
 
-print(predicted_dates)
+
+@app.post("/dates")
+async def calc_dates(data: Request):
+    dates = await data.json()
+    dates = dates['dates']
+    date_objects = [datetime.strptime(date, '%Y-%m-%d') for date in dates]
+    normalized_dates = np.array(
+        [(date - min(date_objects)) / (max(date_objects) - min(date_objects)) for date in date_objects])
+    inputs = Input(shape=(1,))
+    x = Dense(256, activation='relu')(inputs)
+    x = Dropout(0.2)(x)
+    x = Dense(128, activation='relu')(x)
+    x = Dropout(0.2)(x)
+    outputs = Dense(1, activation='linear')(x)
+    model = Model(inputs=inputs, outputs=outputs)
+    model.compile(optimizer=Adam(learning_rate=0.001), loss='mse')
+    model.fit(x=normalized_dates[:-1, np.newaxis], y=normalized_dates[1:, np.newaxis], epochs=500, verbose=0)
+    predicted_dates = []
+    current_date = datetime.now()
+    for _ in range(8):
+        normalized_current_date = (current_date - min(date_objects)) / (max(date_objects) - min(date_objects))
+        predicted_normalized_date = model.predict(np.array([[normalized_current_date]]))[0][0]
+        predicted_date = min(date_objects) + timedelta(
+            days=predicted_normalized_date * (max(date_objects) - min(date_objects)).days)
+        predicted_dates.append(predicted_date.strftime('%Y-%m-%d'))
+        current_date = predicted_date
+    return JSONResponse(predicted_dates)
